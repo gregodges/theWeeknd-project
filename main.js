2e1734091c9aa19b37e1e99cb27695a4e3eb6cb6
Mbirepinte <mickael.birepinte@gmail.com>
2022-10-05 11:16:55 +0200

    Add shop icon and hover on button

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import { w3cHtmlValidator } from 'w3c-html-validator';
 const options = { filename: './index.html' };
 w3cHtmlValidator.validate(options).then(w3cHtmlValidator.reporter);
+
+function toBasketVynil() {
+    const targetElement = document.querySelector(".vynil");
+    targetElement.className = "animate";
+  }
